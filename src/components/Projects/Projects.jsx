09e966d89ce1ce7f4fe906b69d320f85b439da0d
Abhishek Rohilla <157987980+rohillaabhishek@users.noreply.github.com>
2026-05-30 21:44:25 +0530

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -22,7 +22,7 @@ const PROJECTS = [
       "Local Storage",
     ],
     links: [
-    { label: "GitHub", href: "https://rohillaabhishek.github.io/testDubaiCleanV7/", icon: "⌂"  }
+    { label: "Dubai Clean Report Builder", href: "https://rohillaabhishek.github.io/testDubaiCleanV7/", icon: "⌂"  }
     ]
   },
 ];
