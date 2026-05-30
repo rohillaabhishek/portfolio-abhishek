b996838ef6939197336ac30ed89e52d341d4ae21
Abhishek Rohilla <157987980+rohillaabhishek@users.noreply.github.com>
2026-05-30 19:45:33 +0530

    Update contactConstants.js

--- src/constants/contactConstants.js
+++ src/constants/contactConstants.js
@@ -1,4 +1,4 @@
 export const mobileNumber = "+91-7678316930";
 export const email = "rohilla.abhishek02@gmail.com";
 export const github = "https://github.com/rohillaabhishek";
-export const linkedin = "www.linkedin.com/in/rohillaabhishek";
+export const linkedin = "https://www.linkedin.com/in/rohillaabhishek?utm_source=share_via&utm_content=profile&utm_medium=member_android";
